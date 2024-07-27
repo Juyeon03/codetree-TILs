a70d22ee56c4e83341377ddb75ec65a36d01b471
Juyeon03 <julienne03@naver.com>
2024-07-27 17:02:04 +0900

    Title: 100 도달하기, Time: 104ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240727/100 도달하기/reach-100.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+        Scanner s=new Scanner(System.in);
+
+        int arr[]=new int[100];
+        int a=0;
+        arr[0]=1;
+        arr[1]=5;
+
+        for(int i=2;i<100;i++)
+        {
+            arr[i]=arr[i-1]+arr[i-2];
+            a++;
+
+
+            if(arr[i-1]>=100)
+            {
+                break;
+            }
+        }
+
+        for(int i=0;i<=a;i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+
+
+    }
+}
