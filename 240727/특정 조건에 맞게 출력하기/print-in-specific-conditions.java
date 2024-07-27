995eed9590817145b1e2015a6d444fb5437c4d64
Juyeon03 <julienne03@naver.com>
2024-07-27 16:55:47 +0900

    Title: 특정 조건에 맞게 출력하기, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

--- 240727/특정 조건에 맞게 출력하기/print-in-specific-conditions.java
+++ 240727/특정 조건에 맞게 출력하기/print-in-specific-conditions.java
@@ -4,31 +4,41 @@ public class Main
 {
     public static void main(String[] args) 
     {
+        //이거는 잘 모르겠어서 그냥 좀 베낌!!
+        //새로운 배열 생성!!
         Scanner s=new Scanner(System.in);
-        int arr[]=new int[100];
+        int Arr[]=new int[100];
+        int newArr[]=new int[100];
+        int zeroPoint=0;
 
         for(int i=0;i<100;i++)
         {
-            arr[i]=s.nextInt();
+            Arr[i]=s.nextInt();
 
-            if(arr[i]==0)
+            if(Arr[i]==0)
             {
-                continue;
+                //밑에서 나옴!! 100으로 해도 print 안하면 되군...
+                zeroPoint=i;
+                break;
             }
 
-            if(arr[i]%2==0)
+            if(Arr[i]%2==0)
             {
-                int b=arr[i]/=2;
-                System.out.print(b+" ");
+                newArr[i]=Arr[i]/2;
             }
 
-            if(arr[i]%2!=0)
+            if(Arr[i]%2!=0)
             {
-                int c=arr[i]+=3;
-                System.out.print(c+" ");
+                newArr[i]=Arr[i]+3;
             }
         }
-        
+
+        //0이 나오기 전까지의 배열의 인덱스에 저장된 값 출력!!
+        for(int i=0;i<zeroPoint;i++)
+        {
+            System.out.print(newArr[i]+" ");
+        }
+
         //NoSuchElementException은 입력이 예상보다 일찍 종료되어 
         //Scanner가 더 이상 읽을 요소가 없을 때 발생합니다.
     }
