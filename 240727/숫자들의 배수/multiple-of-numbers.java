94ede29e7d24399b82a3dd3372ca8ea5e0ee7849
Juyeon03 <julienne03@naver.com>
2024-07-27 16:17:55 +0900

    Title: 숫자들의 배수, Time: 127ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240727/숫자들의 배수/multiple-of-numbers.java
+++ 240727/숫자들의 배수/multiple-of-numbers.java
@@ -8,25 +8,27 @@ public class Main
 
         int arr[]=new int[10];
         arr[0]=s.nextInt(); 
-         System.out.print(arr[0]+" ");
-         //이게 그냥 첫 항에다가 1,2,3 곱하는건지 전 항에다가 1,2,3 곱하는건지 구분
+        System.out.print(arr[0]+" ");
+        //이게 그냥 첫 항에다가 1,2,3 곱하는건지 전 항에다가 1,2,3 곱하는건지 구분
+        
+        //얜 앞에 빼서 쓰기!!
+        if(arr[0]==5&arr[0]==10)
+            {
+                System.out.print(arr[0]+" ");
+                System.out.print(arr[1]);
+                //break 문은 반복문이나 switch 문 안에서만 가능!!
+            }
 
-            for(int i=1;i<10;i++)
+        else
+        {
+             for(int i=1;i<10;i++)
             {
                 arr[i]=arr[0]*(i+1);
-
-                if(arr[0]==5&arr[0]==10)
-                {
-                    System.out.print(arr[0]+" ");
-                    System.out.print(arr[1]);
-                    break;
-                }
-
-                else
-                {
-                    System.out.print(arr[i]+" ");
-                }
+    
+                System.out.print(arr[i]+" ");
+                
             }
+        }
     
     }
     
