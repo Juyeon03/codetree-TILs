f68d81c234c798f8970a57f49c4b6caf29812212
Juyeon03 <julienne03@naver.com>
2024-07-27 15:28:39 +0900

    Title: 배열에서 원소의 특징, Time: 119ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240727/배열에서 원소의 특징/characteristics-of-elements-in-an-array.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+        Scanner s=new Scanner(System.in);
+
+        int arr[]=new int[10];
+
+        for(int i=0;i<10;i++)
+        {
+            arr[i]=s.nextInt();
+
+            if((arr[i])%3==0)
+            {
+                System.out.print(arr[i-1]); 
+                break;
+            }
+        }
+
+    }
+}
